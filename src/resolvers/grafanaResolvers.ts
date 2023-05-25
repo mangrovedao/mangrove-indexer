360188995b2d7481fbf237985232f28554fcd52e
Peter Kejlberg Madsen <peter@mangrove.exchange>
2023-05-25 15:44:32 +0300

    feat: markets query

--- src/resolvers/grafanaResolvers.ts
+++ src/resolvers/grafanaResolvers.ts
@@ -1,9 +1,6 @@
-import { Arg, Ctx, Query, Resolver } from "type-graphql";
-import { MarketInfo, OfferList } from "./grafanaObjects";
 import { Offer, OfferListing, OfferListingVersion, OfferVersion, PrismaClient, Token } from "@prisma/client";
-import { mangrove } from "@proximaone/stream-schema-mangrove/dist/streams";
-import { off } from "process";
-import { of } from "rxjs";
+import { Arg, Ctx, Query, Resolver } from "type-graphql";
+import { Market, MarketInfo, OfferList } from "./grafanaObjects";
 
 
 type Context = {
@@ -13,6 +10,42 @@ type Context = {
 
 @Resolver()
 export class GrafanaResolver {
+
+    @Query(() => [Market])
+    async markets(
+        @Arg("mangrove") mangrove: string,
+        @Ctx() ctx: Context
+    ): Promise<Market[]> {
+        const offerListings =  (await ctx.prisma.mangrove.findFirst({
+            where: { address: mangrove },
+            include: {
+                offerListings: {
+                    include: {
+                        inboundToken: true,
+                        outboundToken: true,
+                    }
+                }
+            }
+        }))?.offerListings;
+        const uniqueOfferListings = offerListings?.reduce((acc, offerListing) => {
+            const inbound = offerListing.inboundToken.address
+            const outbound = offerListing.outboundToken.address
+            if (!acc.find((ol) => 
+            (ol.token1.address === inbound && ol.token2.address === outbound) ||
+            (ol.token1.address === outbound && ol.token2.address === inbound)
+            )) {
+                acc.push({token1: offerListing.inboundToken, token2: offerListing.outboundToken})
+            }
+            return acc
+        }, [] as { token1: Token, token2: Token }[])
+
+        return uniqueOfferListings?.map((pair) => new Market({
+            token1: pair.token1,
+            token2: pair.token2
+        })) ?? []
+    }
+
+
 
 
     @Query(() => MarketInfo)
@@ -25,52 +58,64 @@ export class GrafanaResolver {
     ): Promise<MarketInfo> {
         const offerListing1 = await this.getOfferListingWithExtraInfo(ctx, base, quote, mangrove)
         const offerListing2 = await this.getOfferListingWithExtraInfo(ctx, quote, base, mangrove)
-        const kandelsDeployed  = await ctx.prisma.kandel.count({ where: { 
-            type: "NewKandel",
-            mangrove: { address: { contains: mangrove.toLowerCase() , mode: "insensitive" } },
-            baseToken: { OR: [ { address: { contains: base.toLowerCase() , mode: "insensitive"}}, { address: { contains: quote.toLowerCase() , mode: "insensitive" } } ] },
-            quoteToken: { OR: [ { address: { contains: base.toLowerCase() , mode: "insensitive"}}, { address: { contains: quote.toLowerCase() , mode: "insensitive" } } ] } 
-        } });
-
-        const aaveKandelsDeployed  = await ctx.prisma.kandel.count({ where: {
-            type: "NewAaveKandel",
-            mangrove: { address: { contains: mangrove.toLowerCase() , mode: "insensitive" } },
-            baseToken: { OR: [ { address: { contains: base.toLowerCase() , mode: "insensitive"}}, { address: { contains: quote.toLowerCase() , mode: "insensitive" } } ] },
-            quoteToken: { OR: [ { address: { contains: base.toLowerCase() , mode: "insensitive"}}, { address: { contains: quote.toLowerCase() , mode: "insensitive" } } ] } 
-        } });
-
-        const activeKandels  = await ctx.prisma.kandel.count({ where: {
-            type: "NewKandel",
-            mangrove: { address: { contains: mangrove.toLowerCase() , mode: "insensitive" } },
-            baseToken: { OR: [ { address: { contains: base.toLowerCase() , mode: "insensitive"}}, { address: { contains: quote.toLowerCase() , mode: "insensitive" } } ] },
-            quoteToken: { OR: [ { address: { contains: base.toLowerCase() , mode: "insensitive"}}, { address: { contains: quote.toLowerCase() , mode: "insensitive" } } ] } ,
-            strat: { offers: { some: { currentVersion: { live: true } } } }
-        } });
-
-        const activeAaveKandels  = await ctx.prisma.kandel.count({ where: {
-            type: "NewAaveKandel",
-            mangrove: { address: { contains: mangrove.toLowerCase() , mode: "insensitive" } },
-            baseToken: { OR: [ { address: { contains: base.toLowerCase() , mode: "insensitive"}}, { address: { contains: quote.toLowerCase() , mode: "insensitive" } } ] },
-            quoteToken: { OR: [ { address: { contains: base.toLowerCase() , mode: "insensitive"}}, { address: { contains: quote.toLowerCase() , mode: "insensitive" } } ] } ,
-            strat: { offers: { some: { currentVersion: { live: true } } } }
-        } });
-
-        const mangroveOrders = await ctx.prisma.mangroveOrder.count({ where: {
-            mangrove: { address: { contains: mangrove.toLowerCase() , mode: "insensitive" } },
-            offerListing: {
-                inboundToken: { OR: [ { address: { contains: base.toLowerCase() , mode: "insensitive"}}, { address: { contains: quote.toLowerCase() , mode: "insensitive" } } ] },
-                outboundToken: { OR: [ { address: { contains: base.toLowerCase() , mode: "insensitive"}}, { address: { contains: quote.toLowerCase() , mode: "insensitive" } } ] },
+        const kandelsDeployed = await ctx.prisma.kandel.count({
+            where: {
+                type: "NewKandel",
+                mangrove: { address: { contains: mangrove.toLowerCase(), mode: "insensitive" } },
+                baseToken: { OR: [{ address: { contains: base.toLowerCase(), mode: "insensitive" } }, { address: { contains: quote.toLowerCase(), mode: "insensitive" } }] },
+                quoteToken: { OR: [{ address: { contains: base.toLowerCase(), mode: "insensitive" } }, { address: { contains: quote.toLowerCase(), mode: "insensitive" } }] }
             }
-        } });
+        });
 
-        const marketOrders = await ctx.prisma.order.count({ where: {
-            mangrove: { address: { contains: mangrove.toLowerCase() , mode: "insensitive" } },
-            MangroveOrder: undefined,
-            offerListing: {
-                inboundToken: { OR: [ { address: { contains: base.toLowerCase() , mode: "insensitive" } }, { address: { contains: quote.toLowerCase() , mode: "insensitive" } } ] },
-                outboundToken: { OR: [ { address: { contains: base.toLowerCase() , mode: "insensitive"}}, { address: { contains: quote.toLowerCase() , mode: "insensitive" } } ] },
+        const aaveKandelsDeployed = await ctx.prisma.kandel.count({
+            where: {
+                type: "NewAaveKandel",
+                mangrove: { address: { contains: mangrove.toLowerCase(), mode: "insensitive" } },
+                baseToken: { OR: [{ address: { contains: base.toLowerCase(), mode: "insensitive" } }, { address: { contains: quote.toLowerCase(), mode: "insensitive" } }] },
+                quoteToken: { OR: [{ address: { contains: base.toLowerCase(), mode: "insensitive" } }, { address: { contains: quote.toLowerCase(), mode: "insensitive" } }] }
             }
-        } });
+        });
+
+        const activeKandels = await ctx.prisma.kandel.count({
+            where: {
+                type: "NewKandel",
+                mangrove: { address: { contains: mangrove.toLowerCase(), mode: "insensitive" } },
+                baseToken: { OR: [{ address: { contains: base.toLowerCase(), mode: "insensitive" } }, { address: { contains: quote.toLowerCase(), mode: "insensitive" } }] },
+                quoteToken: { OR: [{ address: { contains: base.toLowerCase(), mode: "insensitive" } }, { address: { contains: quote.toLowerCase(), mode: "insensitive" } }] },
+                strat: { offers: { some: { currentVersion: { live: true } } } }
+            }
+        });
+
+        const activeAaveKandels = await ctx.prisma.kandel.count({
+            where: {
+                type: "NewAaveKandel",
+                mangrove: { address: { contains: mangrove.toLowerCase(), mode: "insensitive" } },
+                baseToken: { OR: [{ address: { contains: base.toLowerCase(), mode: "insensitive" } }, { address: { contains: quote.toLowerCase(), mode: "insensitive" } }] },
+                quoteToken: { OR: [{ address: { contains: base.toLowerCase(), mode: "insensitive" } }, { address: { contains: quote.toLowerCase(), mode: "insensitive" } }] },
+                strat: { offers: { some: { currentVersion: { live: true } } } }
+            }
+        });
+
+        const mangroveOrders = await ctx.prisma.mangroveOrder.count({
+            where: {
+                mangrove: { address: { contains: mangrove.toLowerCase(), mode: "insensitive" } },
+                offerListing: {
+                    inboundToken: { OR: [{ address: { contains: base.toLowerCase(), mode: "insensitive" } }, { address: { contains: quote.toLowerCase(), mode: "insensitive" } }] },
+                    outboundToken: { OR: [{ address: { contains: base.toLowerCase(), mode: "insensitive" } }, { address: { contains: quote.toLowerCase(), mode: "insensitive" } }] },
+                }
+            }
+        });
+
+        const marketOrders = await ctx.prisma.order.count({
+            where: {
+                mangrove: { address: { contains: mangrove.toLowerCase(), mode: "insensitive" } },
+                MangroveOrder: undefined,
+                offerListing: {
+                    inboundToken: { OR: [{ address: { contains: base.toLowerCase(), mode: "insensitive" } }, { address: { contains: quote.toLowerCase(), mode: "insensitive" } }] },
+                    outboundToken: { OR: [{ address: { contains: base.toLowerCase(), mode: "insensitive" } }, { address: { contains: quote.toLowerCase(), mode: "insensitive" } }] },
+                }
+            }
+        });
 
 
         return new MarketInfo({
